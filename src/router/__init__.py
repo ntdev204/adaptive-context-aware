f43a4d293a36c3e4b7e8bb834feddf817d52b5c5
thien <thien.npc369@gmail.com>
2026-05-11 16:34:43 +0700

    feat: add T2.9 adaptive router

---
+++ src/router/__init__.py
@@ -0,0 +1,5 @@
+"""Adaptive routing policies for Phase 2 reasoning."""
+
+from .adaptive_router import AdaptiveRouter, ReasoningPathway, RoutingDecision
+
+__all__ = ["AdaptiveRouter", "ReasoningPathway", "RoutingDecision"]
